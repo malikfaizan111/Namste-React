1704f0bb2a7964fb4f0a4117b04c92ceac04077b
Faizan Atif <faizan.atif@ircmonline.com>
2024-07-09 20:21:48 +0500

    update episode 2 igniting our app

---
+++ Episode 02 ( Igniting our App )/app.js
@@ -0,0 +1,60 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// const heading = React.createElement("h1", { id: "heading" }, "Hello World From React!");
+
+// console.log(heading); // object & React Element
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+/**
+ * 
+ * 
+ * <div id="parent">
+ *      <div id="child">
+ *         <h1>I'm h1 tag</h1>
+ *      </div>
+ * </div>
+ * 
+ * 
+ * ReactElement(Object) ==> HTML(Browser Understands)
+ * 
+ */
+
+// const parent =
+//     React.createElement("div",
+//         { id: "parent" },
+//         React.createElement("div",
+//             { id: "child" },
+//             [React.createElement("h1", {}, "I'm  an h1 tag"),
+//             React.createElement("h2", {}, "I'm  an h2 tag")
+//             ]
+//         )
+//     );
+
+const parent =
+    React.createElement("div",
+        { id: "parent" },
+        [
+            React.createElement("div",
+                { id: "child" },
+                [React.createElement("h1", {}, "I'm  an h1 tag"),
+                React.createElement("h2", {}, "I'm  an h2 tag")
+                ]
+            ),
+            React.createElement("div",
+                { id: "child2" },
+                [React.createElement("h1", {}, "I'm  an h1 tag"),
+                React.createElement("h2", {}, "I'm  an h2 tag")
+                ]
+            )
+        ]
+    );
+
+// messy code 
+// jsx help is to get rid of this messy code
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent)
---
+++ ReadMe.md
@@ -0,0 +1,23 @@
+...
+# Namste React 🚀
+
+
+...
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - Written in c++
+- Caching - Faster Builders
+- Image Optimization
+- Minification of our files
+- Bundling
+- Compressing
+- Consistent Hashing  (read about this)
+- code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPS
+- Tree Shaking - remove unused code for you
+- Different dev and prod bundles
